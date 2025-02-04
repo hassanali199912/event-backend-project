4791e7a03ca453a423b01009914344cdbabe1279
Hassan Ali Hassan <85422048+hassanali199912@users.noreply.github.com>
2025-02-04 12:52:38 +0200

    events by admins

--- controllers/dashboard/event.js
+++ controllers/dashboard/event.js
@@ -1,6 +1,148 @@
 const ResponseHandler = require("../../utils/ResponseHandler");
 const EventCrud_crud = require("../../services/event-crud");
 const EventModule = new EventCrud_crud();
+const { normalizePath, removeFile } = require("../../utils/FileHelper");
+
+/*
+    The Event Oprations For The Users
+*/
+
+
+// Main Crud      
+const getAllEvents = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const events = await EventModule.getAll();
+        return responseHandler.success(events, "Events Feched Successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+const getEventBiID = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const event = await EventModule.getById(req.params.id);
+        return responseHandler.success(event, "Event Feched Successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+
+// Part 1 The Basic Infromation
+const createEventP1 = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const event = await EventModule.create({
+            ...req.body,
+            userId: req.userId
+        });
+        return responseHandler.success(event, "Event created successfully", 201);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+const updateEventP1 = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const isExist = await EventModule.getById(req.params.id);
+        if (!isExist) {
+            return responseHandler.notFound("Event not found");
+        }
+        const event = await EventModule.update(req.params.id, req.body);
+        return responseHandler.success(event, "Event updated successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+
+
+// Part 2 The Images
+const uploadEventImagesP2 = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const image = req.file;
+        console.log(req.file);
+
+        const { type } = req.body
+        if (!image) {
+            return responseHandler.fileFormatError("Invalid Error image is require and file type (Only .jpg, .jpeg, .png, .webp are allowed)");
+        }
+
+        const event = await EventModule.getById(req.params.id);
+        if (event.images[type] && event.images[type] !== null) {
+            removeFile(event.images[type]);
+        }
+        await EventModule.update(req.params.id, {
+            $set: {
+                [`images.${type}`]: normalizePath(image.path)
+            }
+        });
+        return responseHandler.success(event, "Event updated successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+const deleteEventImagesP2 = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+
+        const { type } = req.headers
+
+
+        const event = await EventModule.getById(req.params.id);
+        if (event.images[type]) {
+            removeFile(event.images[type]);
+        }
+        await EventModule.update(req.params.id, {
+            $set: {
+                [`images.${type}`]: ""
+            }
+        });
+
+
+
+        return responseHandler.success(event, "Event created successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+
+
+
+// Part 3 The Locations
+const uploadEventLocationsP3 = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const event = await EventModule.update(req.params.id, {
+            $set: {
+                map: req.body.map
+            }
+        });
+        return responseHandler.success(event, "Event updated successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+
+
+
+// My events
+const getMyEvents = async (req, res) => {
+    const responseHandler = new ResponseHandler(res);
+    try {
+        const events = await EventModule.filterBy({ userId: req.userId });
+        return responseHandler.success(events, "Events Feched Successfully", 200);
+    } catch (error) {
+        return responseHandler.error(error.message, 500, error);
+    }
+}
+
+
+
+
+
+
+
 
 const sliderSelector = async (req, res) => {
     const responseHandler = new ResponseHandler(res);
@@ -25,5 +167,19 @@ const sliderSelector = async (req, res) => {
 
 
 module.exports = {
-    sliderSelector
+    sliderSelector,
+    // The Basic Crud Oprations
+    getAllEvents,
+    getEventBiID,
+
+    // user events
+    getMyEvents,
+    // Part 1
+    createEventP1,
+    updateEventP1,
+    // Part 2
+    uploadEventImagesP2,
+    deleteEventImagesP2,
+    // Part 3
+    uploadEventLocationsP3,
 }
--- routes/admin/event.js
+++ routes/admin/event.js
@@ -1,9 +1,40 @@
 const express = require("express");
-
 const routes = express.Router();
+const Events = require("../../models/event");
+const { upload } = require("../../utils/MulterImageUploader");
+const { checkToken, isOwner, isAdmin } = require("../../middlewares/AuthVaildator");
+const { eventValidationRulesP1, eventValidationRulesP2, eventValidationRulesP3 } = require("../../middlewares/validations/events");
 
-const { sliderSelector } = require("../../controllers/dashboard/event");
-const { isAdmin } = require("../../middlewares/AuthVaildator");
+const {
+    sliderSelector,
+    getAllEvents,
+    getEventBiID,
+    getMyEvents,
+    createEventP1,
+    updateEventP1,
+    uploadEventImagesP2,
+    deleteEventImagesP2,
+    uploadEventLocationsP3 } = require("../../controllers/dashboard/event");
+
+
+
+
+
+//  The Basic Crud Oprations
+routes.get("/", getAllEvents);
+routes.get("/:id", getEventBiID);
+
+// Part 1 The Basic Infromation
+routes.post("/p1/", isAdmin, eventValidationRulesP1, createEventP1);
+routes.post("/p1/:id", isAdmin, updateEventP1);
+
+// Part 2 The Images
+routes.post("/p2/images/:id", isAdmin, eventValidationRulesP2, upload.single("image"), uploadEventImagesP2);
+routes.delete("/p2/images/:id", isAdmin, deleteEventImagesP2);
+
+// Part 3 The Locations
+routes.post("/p3/map/:id", isAdmin, eventValidationRulesP3, uploadEventLocationsP3);
+
 
 routes.get("/slider/:id"
     //,isAdmin
